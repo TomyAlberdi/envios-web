0e7d78ddf007d9072fa51c1a4df574df9b020d23
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-08-23 21:18:33 -0300

    Started navbar development

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div className="Navbar">
-      navbar
+      <section className="logoName">
+        <img src="/delivery-blue.svg" alt="logo" />
+        <div className="name">
+          <h2>Nombre</h2>
+          <p>Subtítulo</p>
+        </div>
+      </section>
+      <nav>
+        <Link>Inicio</Link>
+        <Link>Nosotros</Link>
+        <Link>Servicios</Link>
+        <Link>Contacto</Link>
+      </nav>
     </div>
-  )
-}
-export default Navbar
+  );
+};
+export default Navbar;

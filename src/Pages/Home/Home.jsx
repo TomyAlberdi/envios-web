9ae103a493eb6c09d70d003c912947d657f743e8
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-08-26 19:01:00 -0300

    Home Header development

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,16 @@
 const Home = () => {
   return (
     <section className="Home">
-      home
+      <div className="header">
+        <h3>Lorem ipsum dolor </h3>
+        <h1>Lorem ipsum dolor sit amet</h1>
+        <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h5>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nisi
+          nunc, ultrices fringilla tempus vel, finibus vel tortor.
+        </p>
+      </div>
     </section>
-  )
-}
-export default Home
+  );
+};
+export default Home;

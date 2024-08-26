6aab4abc9beaa6390273fca62c01749fe4de0b4e
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-08-26 17:33:02 -0300

    Started footer development

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,33 @@
+import { Link } from "react-router-dom";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+
 const Footer = () => {
   return (
     <footer>
-      footer
+      <div className="menu">
+        <Link to={"/"}>
+          <IoIosArrowDroprightCircle />
+          <span>Inicio</span>
+        </Link>
+        <Link to={"/nosotros"}>
+          <IoIosArrowDroprightCircle />
+
+          <span>Nosotros</span>
+        </Link>
+        <Link to={"/servicios"}>
+          <IoIosArrowDroprightCircle />
+
+          <span>Servicios</span>
+        </Link>
+        <Link to={"/contacto"}>
+          <IoIosArrowDroprightCircle />
+
+          <span>Contacto</span>
+        </Link>
+      </div>
+      <div className="services">services</div>
+      <div className="contact">contact</div>
     </footer>
-  )
-}
-export default Footer
+  );
+};
+export default Footer;

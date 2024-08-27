13bd0b2b5d97e4d7ef21fddd632526327ac6d5a6
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-08-27 17:08:11 -0300

    Added ScrollToTop Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
+import ScrollButton from "./Components/ScrollButton/ScrollButton.jsx";
 import CustomRouter from "./routes.jsx";
 
 function App() {
   return (
     <div className="App">
       <CustomRouter />
+      <ScrollButton />
     </div>
   );
 }

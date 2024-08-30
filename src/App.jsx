f82d05528c9c55ea2c6ccf7d061bfa6028e5ecd4
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-08-30 17:24:00 -0300

    Added Welcome Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 import ScrollButton from "./Components/ScrollButton/ScrollButton.jsx";
 import CustomRouter from "./routes.jsx";
+import WelcomeScreen from "./Components/WelcomeScreen/WelcomeScreen.jsx";
 
 function App() {
+  
+  const [Loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3750);
+  }, []);
+
   return (
-    <div className="App">
+    <div className={"App" + (Loading ? " Loading" : "")}> 
+      <WelcomeScreen loading={Loading} />
       <CustomRouter />
       <ScrollButton />
     </div>
